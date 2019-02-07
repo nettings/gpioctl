b782f82458c1a820f15638097be4d0f0fa8eb63a
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2019-02-07 20:31:44 +0000

    work on both channels of a mixer element, use dB, operate on handle, not name.

--- alsa_process.c
+++ alsa_process.c
@@ -13,30 +13,53 @@ void setup_ALSA() {
         snd_mixer_attach(handle, card);
         snd_mixer_selem_register(handle, NULL, NULL);
         snd_mixer_load(handle);
-
 }
 
 void shutdown_ALSA() {
     snd_mixer_close(handle);
 }
 
-int alsa_set_mixer(char *mixer_scontrol, int step, int val) {
-        long setting, min, max;
+snd_mixer_elem_t* setup_ALSA_mixer_handle(char *mixer_scontrol) {
         char name[64] = {0};
         snd_mixer_selem_id_t *sid;
-
+        snd_mixer_elem_t* elem;
         strncpy(name, mixer_scontrol, 64);
-
         snd_mixer_selem_id_alloca(&sid);
         snd_mixer_selem_id_set_index(sid, 0);
         snd_mixer_selem_id_set_name(sid, name);
-        snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
-        
-        snd_mixer_selem_get_playback_dB_range(elem, &min, &max);
-        snd_mixer_selem_get_playback_dB(elem, 0xffff, &setting);         
-        int err = snd_mixer_selem_set_playback_dB_all(elem, setting + (step * val), val);
-        ERR("alsa error: %s for setting %d", snd_strerror(err), setting + (step * val));
+        elem = snd_mixer_find_selem(handle, sid);
+        if (elem == NULL) {
+                ERR("ALSA error: could not find mixer simple element %s.", mixer_scontrol);
+        }
+        return elem;
 }
 
-int alsa_toggle_mute(char * mixer_scontrol){
+int set_ALSA_mixer_elem(snd_mixer_elem_t* elem, int step, int val) {
+        int err;
+        const int nch = 2;
+        const int chlist[2] = { SND_MIXER_SCHN_FRONT_LEFT, SND_MIXER_SCHN_FRONT_RIGHT };
+        long chval[2] = {42};
+        long min = 0;
+        long max = 0;
+        
+        snd_mixer_selem_get_playback_dB_range(elem, &min, &max);
+        for (int i=0; i<nch; i++) {
+                err = snd_mixer_selem_get_playback_dB(elem, chlist[i], &chval[i]);         
+                if (err) {
+                        ERR("ALSA error getting value for %s ch %d: %s.", snd_mixer_selem_get_name(elem), i, snd_strerror(err));
+                } else {
+                        DBG("ALSA reports current setting for %s ch %d as %d milliBel [%d..%d]", snd_mixer_selem_get_name(elem), i, chval[i], min, max);
+                }
+                chval[i] += (step * val * 100);
+                err = snd_mixer_selem_set_playback_dB(elem, chlist[i], chval[i], val);
+                if (err) {
+                        ERR("ALSA error: %s while setting %s to %d milliBel", snd_strerror(err), snd_mixer_selem_get_name(elem), chval[i]);
+                } else {
+                        DBG("ALSA reports %s while setting %s to %d milliBel", snd_strerror(err), snd_mixer_selem_get_name(elem), chval[i]);
+                }
+        }
 }
+
+int toggle_ALSA_mute(snd_mixer_elem_t* elem){
+}
+
--- alsa_process.h
+++ alsa_process.h
@@ -1,10 +1,11 @@
 #ifndef ALSA_PROCESS_H
 #define ALSA_PROCESS_H
+#include <alsa/asoundlib.h>
 
-int alsa_set_mixer(char * mixer_scontrol, int step, int val);
-int alsa_toggle_mute(char * mixer_scontrol);
 void setup_ALSA();
 void shutdown_ALSA();
+snd_mixer_elem_t* setup_ALSA_mixer_handle(char *mixer_scontrol);
+int set_ALSA_mixer_elem(snd_mixer_elem_t* elem, int step, int val);
+int toggle_ALSA_mute(snd_mixer_elem_t* elem);
 
 #endif
-
