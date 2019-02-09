7130fb047cbbe75958b7036df9ad751c421afd5f
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2019-02-09 17:47:31 +0000

    add conditional compile ifdefs (and learn the hard way how ugly they are).

--- parse_cmdline.c
+++ parse_cmdline.c
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
+#include "config.h"
 #include "globals.h"
 
 #define MAXARG 5
@@ -28,27 +29,23 @@
 void usage()
 {
 	printf
-	    ("\n%s handles switches and rotary encoders connected to GPIOs, using\n",
+	    ("\n%s handles switches and rotary encoders connected to GPIOs, using the\n",
 	     JACK_CLIENT_NAME);
 	printf
-	    ("the portable libgpiod kernel interface, to create JACK MIDI CC messages\n");
-	printf("at %s:%s or directly interact with an ALSA mixer control.\n",
+	    ("portable libgpiod kernel interface, to create JACK MIDI CC messages at\n");
+	printf("%s:%s or directly interact with an ALSA mixer control.\n",
 	       JACK_CLIENT_NAME, JACK_PORT_NAME);
 	printf
-	    ("All GPI pins are pulled up, so the return should be connected to ground.\n\n");
+	    ("We assume GPI pins have a pull-up, so the return should be connected to ground.\n");
 	printf("-h|--help      This help.\n");
-	printf("-v|--verbose   Print current controller values.\n");
+	printf("-v|--verbose   Print current controller values.\n\n");
+	printf("The following options may be specified multiple times:\n");
+#ifdef HAVE_JACK
 	printf("-J|--jack-rotary clk,dt,ch,cc,step\n");
 	printf
 	    ("               Set up a rotary encoder on pins [clk] and [dt], and create MIDI\n");
 	printf
 	    ("               messages on channel [ch] for CC no. [cc] with step size [step].\n");
-	printf("-A|--amixer-rotary clk,dt,name,step\n");
-	printf
-	    ("               Set up a rotary encoder on pins [clk] and [dt], and control\n");
-	printf
-	    ("               ALSA mixer element [name] with step size [step].\n");
-	printf("               Use 'amixer scontrols' to get a list.\n");
 	printf("-j|--jack-switch sw,ch,cc,toggled\n");
 	printf
 	    ("               Set up a switch on pin [sw], and create MIDI messages on channel\n");
@@ -59,17 +56,31 @@ void usage()
 	printf
 	    ("               at 1, one press will latch it to '127', and the next one will\n");
 	printf("               release it to '0'.\n");
+#endif
+#ifdef HAVE_ALSA
+	printf("-A|--amixer-rotary clk,dt,name,step\n");
+	printf
+	    ("               Set up a rotary encoder on pins [clk] and [dt], and control\n");
+	printf
+	    ("               ALSA mixer element [name] with step size [step].\n");
+	printf("               Use 'amixer scontrols' to get a list.\n");
 	printf("-a|--amixer-mute sw,name\n");
 	printf
 	    ("               Set up a switch on pin [sw], and toggle the MUTE status on ALSA\n");
-	printf("               mixer element [name].\n\n");
-	printf("The options [JAja] may be specified multiple times.\n\n");
+	printf("               mixer element [name].\n");
+#endif	
 	printf
-	    ("Pin numbers below are hardware GPIO numbers. They do not usually correspond\n");
+	    ("Pin numbers above are hardware GPIO numbers. They do not usually correspond\n");
 	printf
 	    ("to physical pin numbers. For the RPi, check https://pinout.xyz/# and look\n");
 	printf
-	    ("for the Broadcom ('BCM') numbers.\n\n");
+	    ("for the Broadcom ('BCM') numbers.\n");
+	printf
+	    ("libgpiod does not know how to control the pull-up/pull-down resistors of your\n");
+	printf
+	    ("GPIO pins. Use an external tool to enable them, or add physical pull-ups.\n\n");
+	printf
+	    ("%s is meant to run as a daemon. Use CTRL-C or send it SIGTERM to exit.\n\n", JACK_CLIENT_NAME);
 }
 
 static int tokenize(char *argument, char *config[])
@@ -91,19 +102,27 @@ int parse_cmdline(int argc, char *argv[])
 	int i;
 	int line;
 	char *config[MAXARG] = { 0 };
+#ifdef HAVE_JACK
 	jack_rotary_t *jrdata;
-	amixer_rotary_t *ardata;
 	jack_switch_t *jsdata;
+#endif
+#ifdef HAVE_ALSA
+	amixer_rotary_t *ardata;
 	amixer_mute_t *amdata;
+#endif
 
 	while (1) {
 		static struct option long_options[] = {
 			{"help", no_argument, 0, 'h'},
 			{"verbose", no_argument, 0, 'v'},
+#ifdef HAVE_JACK
 			{"jack-rotary", required_argument, 0, 'J'},
-			{"amixer-rotary", required_argument, 0, 'A'},
 			{"jack-switch", required_argument, 0, 'j'},
+#endif
+#ifdef HAVE_ALSA
+			{"amixer-rotary", required_argument, 0, 'A'},
 			{"amixer-mute", required_argument, 0, 'a'},
+#endif
 			{0, 0, 0, 0}
 		};
 		int optind = 0;
@@ -119,6 +138,7 @@ int parse_cmdline(int argc, char *argv[])
 		case 'v':
 			verbose = 1;
 			break;
+#ifdef HAVE_JACK
 		case 'J':
 			i = tokenize(optarg, config);
 			if (i != 5) {
@@ -139,25 +159,6 @@ int parse_cmdline(int argc, char *argv[])
 			jrdata->midi_cc = atoi(config[3]);
 			jrdata->step = atoi(config[4]);
 			break;
-		case 'A':
-			i = tokenize(optarg, config);
-			if (i != 4) {
-				ERR("-A needs exactly 4 options.");
-				return EXIT_ERR;
-			}
-			ardata =
-			    (amixer_rotary_t *) malloc(sizeof(amixer_rotary_t));
-			if (ardata == NULL) {
-				ERR("malloc() failed.");
-				return EXIT_ERR;
-			}
-			line = ardata->clk = atoi(config[0]);
-			controllers[line].type = ALSAROT;
-			controllers[line].data = ardata;
-			ardata->dt = atoi(config[1]);
-			strncpy(ardata->mixer_scontrol, config[2], MAXNAME);
-			ardata->step = atoi(config[3]);
-			break;
 		case 'j':
 			i = tokenize(optarg, config);
 			if (i != 4) {
@@ -177,6 +178,27 @@ int parse_cmdline(int argc, char *argv[])
 			jsdata->midi_cc = atoi(config[2]);
 			jsdata->toggled = atoi(config[3]);
 			break;
+#endif
+#ifdef HAVE_ALSA
+		case 'A':
+			i = tokenize(optarg, config);
+			if (i != 4) {
+				ERR("-A needs exactly 4 options.");
+				return EXIT_ERR;
+			}
+			ardata =
+			    (amixer_rotary_t *) malloc(sizeof(amixer_rotary_t));
+			if (ardata == NULL) {
+				ERR("malloc() failed.");
+				return EXIT_ERR;
+			}
+			line = ardata->clk = atoi(config[0]);
+			controllers[line].type = ALSAROT;
+			controllers[line].data = ardata;
+			ardata->dt = atoi(config[1]);
+			strncpy(ardata->mixer_scontrol, config[2], MAXNAME);
+			ardata->step = atoi(config[3]);
+			break;
 		case 'a':
 			i = tokenize(optarg, config);
 			if (i != 2) {
@@ -194,6 +216,17 @@ int parse_cmdline(int argc, char *argv[])
 			controllers[line].data = amdata;
 			strncpy(amdata->mixer_scontrol, config[1], MAXNAME);
 			break;
+#endif
+		case 'X':
+			i = tokenize(optarg, config);
+			if (i != 3) {
+				ERR("-X needs exactly 3 options.");
+				return EXIT_ERR;
+			}
+			
+		default:
+			ERR("Unknown option or feature not compiled in.");
+			return EXIT_ERR;
 		}
 	}
 	if (argc < 3) {
