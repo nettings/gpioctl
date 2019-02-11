3b2738fe0ef1f5c2b34a57bd7bdc8c35f9eeb8b5
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2019-02-11 18:26:54 +0000

    shorten help text, consistent use of local vars, parsing fixes

--- parse_cmdline.c
+++ parse_cmdline.c
@@ -37,10 +37,10 @@ void usage()
 	printf("We assume GPI pins have a pull-up, so the return should be connected to ground.\n");
 	printf("-h|--help      This help.\n");
 	printf("-v|--verbose   Print current controller values.\n\n");
-	printf("The following options may be specified multiple times:\n\n");
+	printf("The following options may be specified multiple times. All parameters must be\n");
+	printf("separated by commas, no spaces.\n\n");
 	printf("-r|--rotary clk,dt,type,...\n");
-	printf("               Set up a rotary encoder. All parameters must be separated\n");
-	printf("               by commas, no spaces.\n");
+	printf("               Set up a rotary encoder.\n");
 	printf("               clk:     the GPI number of the first encoder contact (0-%d)\n", MAXGPIO);
 	printf("               dt:      the GPI number of the second encoder contact (0-%d)\n", MAXGPIO);
         printf("               Depending on 'type', other options must follow:\n\n");
@@ -54,21 +54,20 @@ void usage()
 	printf("               default: the initial value, default is 'min'\n\n"); 
 #endif
 #ifdef HAVE_ALSA
-	printf("      ...,alsa,control[,card[,step]]\n");
+	printf("      ...,alsa,control[,step],card]\n");
 	printf("               control: the name of a simple controller in ALSA mixer\n");
-	printf("               card:    the name of a sound interface (defaults to hw:0)\n");
 	printf("               step: the step size in dB per click, default 3\n\n");
+	printf("               card:    the name of a sound interface (defaults to hw:0)\n");
 #endif
 	printf("      ...,stdout,format[,min[,max[,step[,default]]]]].\n");
-	printf("               format:  a string that can contain the special tokens '%%pin%%'\n");
+	printf("               format:  a string that can contain the special tokens '%%gpi%%'\n");
 	printf("                        (the pin number) and '%%val%%' (the value)\n");
 	printf("               min:     minimum value (%d-%d), default 0\n", INT_MIN, INT_MAX);
 	printf("               max:     maximum value (%d-%d), default 100\n", INT_MIN, INT_MAX);
 	printf("               step:    the step size per click, default 1\n"); 
 	printf("               default:	the initial value, default is 'min'\n\n");
 	printf("-s|--switch sw,type...\n");
-	printf("               Set up a switch. Again, all following parameters must be\n");
-	printf("               separated only by commas:\n");
+	printf("               Set up a switch.\n");
 	printf("               sw:      the GPI pin number of the switch contact (0-%d)\n", MAXGPIO);
         printf("               Depending on 'type', other options must follow:\n\n");
 #ifdef HAVE_JACK
@@ -87,7 +86,7 @@ void usage()
 	printf("               card:    the name of a sound interface (defaults to hw:0)\n\n");
 #endif
 	printf("      ...,stdout,format[,latch[,min[,max[,default]]]]\n");
-	printf("               format:  a string that can contain the special tokens '%%pin%%'\n");
+	printf("               format:  a string that can contain the special tokens '%%gpi%%'\n");
 	printf("                        (the pin number) and '%%val%%' (the value)\n");
 	printf("               latch:   can be 0 (momentary on) or 1 (toggled on/off)\n");
 	printf("               min:     minimum value (%d-%d), default 0\n", INT_MIN, INT_MAX);
@@ -124,11 +123,12 @@ static int match(char *string1, char *string2) {
 
 int parse_cmdline(int argc, char *argv[])
 {
-	int c;
+	int o;
 	int i;
 	char *config[MAXARG];
 
-	control_t* data;
+	control_t* c = NULL;
+	control_t* d = NULL;
 
 	static struct option long_options[] = {
 		{"help", no_argument, 0, 'h'},
@@ -140,11 +140,11 @@ int parse_cmdline(int argc, char *argv[])
 	
 	while (1) {
 		int optind = 0;
-		c = getopt_long(argc, argv, "hvr:s:", long_options,
+		o = getopt_long(argc, argv, "hvr:s:", long_options,
 				&optind);
-		if (c == -1)
+		if (o == -1)
 			break;
-		switch (c) {
+		switch (o) {
 		case 'h':
 			return EXIT_USAGE;
 			break;
@@ -153,85 +153,87 @@ int parse_cmdline(int argc, char *argv[])
 			break;
 		case 'r':
 			i = tokenize(optarg, config);
-			data = (control_t*) malloc(sizeof(control_t));
-			if (data == NULL) {
-				ERR("malloc() failed.");
-				return EXIT_ERR;
+			c = (control_t*) calloc(sizeof(control_t), 1);
+			d = (control_t*) calloc(sizeof(control_t), 1);
+			if (c == NULL || d == NULL) {
+				ERR("calloc() failed.");
+				goto error;
 			}
 			if (i < 4) {
 				ERR("Not enough options for -r.");
 				goto error;
 			}
-			data->pin1 = atoi(config[0]);
-			if (data->pin1 < 0 || data->pin1 > MAXGPIO) {
+			c->pin1 = atoi(config[0]);
+			if (c->pin1 < 0 || c->pin1 > MAXGPIO) {
 				ERR("clk value out of range.");
 				goto error;
 			}
-			if (controller[data->pin1] != NULL) {
+			if (controller[c->pin1] != NULL) {
 				ERR("clk pin already assigned.");
 				goto error;
 			}
-			data->pin2 = atoi(config[1]);
-			if (data->pin2 < 0 || data->pin2 > MAXGPIO) {
+			c->pin2 = atoi(config[1]);
+			if (c->pin2 < 0 || c->pin2 > MAXGPIO) {
 				ERR("dt value of of range.");
 				goto error;
 			}
-			if (controller[data->pin2] != NULL) {
+			if (controller[c->pin2] != NULL) {
 				ERR("dt pin already assigned.");
 				goto error;
 			}
-			controller[data->pin1] = data;
-			controller[data->pin2] = data;
-			data->type = ROTARY;
+			controller[c->pin1] = c;
+			c->type = ROTARY;
+			controller[c->pin2] = d;
+			d->type = AUX;			
 #ifdef HAVE_JACK
 			if (match(config[2], "jack")) {
-				data->target = JACK;
-				data->midi_cc = atoi(config[3]);
-				if (data->midi_cc < 0 || data->midi_cc > MAXCC) {
+				c->target = JACK;
+				c->midi_cc = atoi(config[3]);
+				if (c->midi_cc < 0 || c->midi_cc > MAXCC) {
 					ERR("MIDI CC value out of range.");
 					goto error;
 				}
 				if (config[4] == NULL) {
-					data->midi_ch = 0;
+					c->midi_ch = 0;
 				} else {
-					data->midi_ch = atoi(config[4]) - 1;
-					if (data->midi_ch < 0 || data->midi_ch > MAXMIDICH) {
+					c->midi_ch = atoi(config[4]) - 1;
+					if (c->midi_ch < 0 || c->midi_ch > MAXMIDICH) {
 						ERR("MIDI channel value out of range.");
 						goto error;
 					}
 				}		
 				if (config[5] == NULL) {
-					data->min = 0;
+					c->min = 0;
 				} else {
-					data->min = atoi(config[5]);
-					if (data->min < 0 || data->min > MAXCCVAL) {
+					c->min = atoi(config[5]);
+					if (c->min < 0 || c->min > MAXCCVAL) {
 						ERR("min value out of range.");
 						goto error;
 					}
 				}
 				if (config[6] == NULL) {
-					data->max = MAXCCVAL;
+					c->max = MAXCCVAL;
 				} else {
-					data->max = atoi(config[6]);
-					if (data->max < 0 || data->min > MAXCCVAL) {
+					c->max = atoi(config[6]);
+					if (c->max < 0 || c->min > MAXCCVAL) {
 						ERR("max value out of range.");
 						goto error;
 					}
 				}
 				if (config[7] == NULL) {
-					data->step = 1;
+					c->step = 1;
 				} else {
-					data->step = atoi(config[7]);
-					if (data->step < 1 || data->step > MAXCCVAL) {
+					c->step = atoi(config[7]);
+					if (c->step < 1 || c->step > MAXCCVAL) {
 						ERR("step value out of range.");
 						goto error;
 					}
 				}
 				if (config[8] == NULL) {
-					data->value = data->min;
+					c->value = c->min;
 				} else {
-					data->value = atoi(config[8]);
-					if (data->value < data->min || data->value > data->max) {
+					c->value = atoi(config[8]);
+					if (c->value < c->min || c->value > c->max) {
 						ERR("default value out of range.");
 						goto error;
 					}
@@ -245,19 +247,19 @@ int parse_cmdline(int argc, char *argv[])
 #endif
 #ifdef HAVE_ALSA			 
 			if (match(config[2], "alsa")) {
-				data->target = ALSA;
-				data->param1 = calloc(sizeof(char), MAXNAME);
-				data->param1 = strncpy(data->param1, config[3], MAXNAME);
-				data->param2 = calloc(sizeof(char), MAXNAME);
+				c->target = ALSA;
+				c->param1 = calloc(sizeof(char), MAXNAME);
+				c->param1 = strncpy(c->param1, config[3], MAXNAME);
 				if (config[4] == NULL) {
-					data->param2 = "hw:0";
+					c->step = 3;
 				} else {
-					data->param2 = strncpy(data->param2, config[4], MAXNAME);
+					c->step = atoi(config[4]);
 				}
+				c->param2 = calloc(sizeof(char), MAXNAME);
 				if (config[5] == NULL) {
-					data->step = 3;
+					c->param2 = "hw:0";
 				} else {
-					data->step = atoi(config[5]);
+					c->param2 = strncpy(c->param2, config[4], MAXNAME);
 				}
 				if (config[6] != NULL) {
 					ERR("Too many arguments.");
@@ -266,40 +268,39 @@ int parse_cmdline(int argc, char *argv[])
 			} else
 #endif			 
 			if (match(config[2], "stdout")) {	
-				data->target = STDOUT;
-				data->param1 = calloc(sizeof(char), MAXNAME);
-				data->param1 = strncpy(data->param1, config[3], MAXNAME);
+				c->target = STDOUT;
+				c->param1 = calloc(sizeof(char), MAXNAME);
+				c->param1 = strncpy(c->param1, config[3], MAXNAME);
 				// TODO: check for presence of %% tokens instead!
-				if (strlen(data->param1) < 1) {
+				if (strlen(c->param1) < 1) {
 					ERR("format cannot be empty.");
 					goto error;					
 				}
 				if (config[4] == NULL) {
-					data->min = 0;
+					c->min = 0;
 				} else {
-					data->min = atoi(config[4]);
+					c->min = atoi(config[4]);
 				}
 				if (config[5] == NULL) {
-					data->max = 100;
+					c->max = 100;
 				} else {
-					data->max = atoi(config[5]);
+					c->max = atoi(config[5]);
 				}
 				if (config[6] == NULL) {
-					data->step = 1;
+					c->step = 1;
 				} else {
-					data->step = atoi(config[6]);
+					c->step = atoi(config[6]);
 				}
 				if (config[7] == NULL) {
-					data->value = data->min;
+					c->value = c->min;
 				} else {
-					data->value = atoi(config[7]);
+					c->value = atoi(config[7]);
 				}
 				if (config[8] != NULL) {
 					ERR("Too many arguments.");
 				}
 				DBG("Parsed control type=%d pin1=%d pin2=%d target=%d min=%d max=%d step=%d default=%d.",
-					data->type, data->pin1, data->pin2, data->target, data->min, data->max, data->step,
-					data->value);
+					c->type, c->pin1, c->pin2, c->target, c->min, c->max, c->step, c->value);
 			} else {
 				ERR("Unknown type '%s'.", config[2]);
 				goto error;
@@ -318,7 +319,8 @@ int parse_cmdline(int argc, char *argv[])
 	}
 	return EXIT_CLEAN;
 	error: {
-		free(data);
+		if (c != NULL) free(c);
+		if (d != NULL) free(d);
 		return EXIT_ERR;
 	}
 }
