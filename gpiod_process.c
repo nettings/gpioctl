8e8be4078f33e28fe5a81717ed59644da76f1415
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2019-02-11 11:52:37 +0000

    simplify gpio data structure, avoid enum clashes

--- gpiod_process.c
+++ gpiod_process.c
@@ -29,27 +29,27 @@
 #define NEVER 0
 #define NOAUX -1
 #define MAXNAME 64
+// debounce time windows, in ms:
+#define GPI_DEBOUNCE_SWITCH 100
+#define GPI_DEBOUNCE_ROTARY 20
+
+typedef enum  {
+	GPI_AUX = -1,		// used as secondary port for other line, no separate interrupt handler
+	GPI_FREE = 0,
+	GPI_ROTARY = 1,
+	GPI_SWITCH = 2
+} line_type_t;
 
 typedef struct {
+	line_type_t type;
 	int aux;
 	unsigned long ts_last;
 	int ts_delta;
 	int (*cb) ();
 } line_t;
 
-static enum line_types {
-	AUX_ = -1,		// used as secondary port for other line, no separate interrupt handler
-	FREE_ = 0,
-	ROTARY_ = 1,
-	SWITCH_ = 2
-};
 
-typedef struct {
-	enum line_types type;
-	line_t *data;
-} line_handler_t;
-
-static line_handler_t line_handler[MAXGPIO] = { 0 };
+static line_t* gpi[MAXGPIO] = { 0 };
 
 static char consumer[MAXNAME];
 static char device[MAXNAME];
@@ -64,35 +64,34 @@ int callback(int event, unsigned int line, const struct timespec *timestamp,
 {
 	int clk, dt, sw;
 	unsigned long now;
-	line_t *d = line_handler[line].data;
 	now = msec_stamp(*timestamp);
-	if ((now - d->ts_last) > d->ts_delta) {
+	if ((now - gpi[line]->ts_last) > gpi[line]->ts_delta) {
 		// we're not bouncing:
-		switch (line_handler[line].type) {
-		case ROTARY_:
+		switch (gpi[line]->type) {
+		case GPI_ROTARY:
 			clk =
 			    (event ==
 			     GPIOD_CTXLESS_EVENT_CB_RISING_EDGE) ? 1 : 0;
-			dt = gpiod_ctxless_get_value(device, d->aux,
+			dt = gpiod_ctxless_get_value(device, gpi[line]->aux,
 						     ACTIVE_HIGH, consumer);
 			if (clk != dt) {
-				d->cb(line, 1);
+				gpi[line]->cb(line, 1);
 			} else {
-				d->cb(line, -1);
+				gpi[line]->cb(line, -1);
 			}
 			break;
-		case SWITCH_:
+		case GPI_SWITCH:
 			sw = (event ==
 			      GPIOD_CTXLESS_EVENT_CB_FALLING_EDGE) ? 1 : 0;
-			d->cb(line, sw);
+			gpi[line]->cb(line, sw);
 			break;
 		default:
 			ERR("No handler for type %d. THIS SHOULD NEVER HAPPEN.",
-			    line_handler[line].type);
+			    gpi[line]->type);
 			return GPIOD_CTXLESS_EVENT_CB_RET_ERR;
 			break;
 		}
-		d->ts_last = now;
+		gpi[line]->ts_last = now;
 	}
 	return GPIOD_CTXLESS_EVENT_CB_RET_OK;
 }
@@ -104,57 +103,43 @@ int null_callback(int event, unsigned int line, const struct timespec *timeout,
 	return GPIOD_CTXLESS_EVENT_CB_RET_ERR;
 }
 
-void setup_gpiod_rotary(int clk, int dt, void (*user_callback))
+void setup_gpiod_rotary(int line, int aux, void (*user_callback))
 {
-	DBG("int clk=%d, int dt=%d, void (*user_callback)=%p", clk, dt,
+	DBG("int line=%d, int aux=%d, void (*user_callback)=%p", line, aux,
 	    user_callback);
-	if (line_handler[clk].type != FREE_) {
-		ERR("Line %d is already in use: %d.", clk,
-		    line_handler[clk].type);
+	if (gpi[line]->type != GPI_FREE) {
+		ERR("Line %d is already in use: %d.", line,
+		    gpi[line]->type);
 		return;
 	}
-	if (line_handler[dt].type != FREE_) {
-		ERR("Aux %d is already in use: %d.", dt, line_handler[dt].type);
+	if (gpi[aux]->type != GPI_FREE) {
+		ERR("Aux %d is already in use: %d.", aux, gpi[aux]->type);
 		return;
 	}
-	if (clk == dt) {
-		ERR("Line and Aux line cannot both be %d.", clk);
+	if (line == aux) {
+		ERR("Line and Aux line cannot both be %d.", line);
 		return;
 	}
-	line_t *d = malloc(sizeof(line_t));
-	if (d == NULL) {
-		ERR("malloc() failed.");
-		return;
-	}
-	line_handler[clk].type = ROTARY_;
-	line_handler[dt].type = AUX_;
-	line_handler[dt].data = NULL;
-	line_handler[clk].data = d;
-	d->aux = dt;
-	d->ts_last = NEVER;
-	d->ts_delta = 20;
-	d->cb = user_callback;
+	gpi[line]->type = GPI_ROTARY;
+	gpi[aux]->type = GPI_AUX;
+	gpi[line]->aux = aux;
+	gpi[line]->ts_last = NEVER;
+	gpi[line]->ts_delta = GPI_DEBOUNCE_ROTARY;
+	gpi[line]->cb = user_callback;
 }
 
-void setup_gpiod_switch(int sw, void (*user_callback))
+void setup_gpiod_switch(int line, void (*user_callback))
 {
-	if (line_handler[sw].type != FREE_) {
-		ERR("Line %d is already in use: %d.", sw,
-		    line_handler[sw].type);
+	if (gpi[line]->type != GPI_FREE) {
+		ERR("Line %d is already in use: %d.", line,
+		    gpi[line]->type);
 		return;
 	}
-	line_t *d = malloc(sizeof(line_t));
-	if (d == NULL) {
-		ERR("malloc() failed.");
-		return;
-	}
-	line_handler[sw].type = SWITCH_;
-	line_handler[sw].data = d;
-
-	d->aux = NOAUX;
-	d->ts_last = NEVER;
-	d->ts_delta = 100;
-	d->cb = user_callback;
+	gpi[line]->type = GPI_SWITCH;
+	gpi[line]->aux = NOAUX;
+	gpi[line]->ts_last = NEVER;
+	gpi[line]->ts_delta = GPI_DEBOUNCE_SWITCH;
+	gpi[line]->cb = user_callback;
 }
 
 void shutdown_gpiod()
@@ -162,12 +147,8 @@ void shutdown_gpiod()
 	gpiod_ctxless_event_loop_multiple(device, NULL, 0, ACTIVE_HIGH,
 					  consumer, FOREVER, NULL,
 					  &null_callback, NULL);
-	for (int i = 0; i < MAXGPIO; i++) {
-		line_handler[i].type == FREE_;
-		if (line_handler[i].data != NULL) {
-			free(line_handler[i].data);
-			line_handler[i].data == NULL;
-		}
+	for (int line = 0; line < MAXGPIO; line++) {
+		gpi[line]->type == GPI_FREE;
 	}
 }
 
@@ -176,14 +157,15 @@ void setup_gpiod_handler(char *dev, char *cons)
 	unsigned int offsets[MAXGPIO];
 	int num_lines = 0;
 	int err = 0;
-	for (int i = 0; i < MAXGPIO; i++) {
-		if (line_handler[i].type > FREE_) {
-			DBG("Added Pin %d in position %d.", i, num_lines);
-			offsets[num_lines++] = i;
+	for (int line = 0; line < MAXGPIO; line++) {
+		if (gpi[line]->type > GPI_FREE) {
+			DBG("Added Pin %d in position %d.", line, num_lines);
+			offsets[num_lines++] = line;
 		}
 	}
 	strncpy(consumer, cons, MAXNAME);
 	strncpy(device, dev, MAXNAME);
+	errno = 0;
 	err = gpiod_ctxless_event_loop_multiple(device, offsets, num_lines,
 						ACTIVE_HIGH, consumer, FOREVER,
 						NULL, callback, NULL);
