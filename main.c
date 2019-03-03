nettings/gpioctl

commit 7379c179b74c617701507748c78a35ba7688f38f

fix unused var when alsa is disabled, remove no-op conditionals

main.c
@@ -142,7 +142,6 @@ void handle_gpi(int line, int val)
 int main(int argc, char *argv[])
 {
 	control_t *c;
-	void *p;
 
 	int rval = parse_cmdline(argc, argv);
 	if (rval != EXIT_CLEAN) {
@@ -184,21 +183,14 @@ int main(int argc, char *argv[])
 			 ERR("Unknown c->type %d. THIS SHOULD NEVER HAPPEN.", c->type);
 		}
 		switch (c->target) {
-#ifdef HAVE_JACK
-		case JACK:
-			break;
-#endif
 #ifdef HAVE_ALSA
 		case ALSA:
-			p = setup_ALSA_mixer_elem(c->param1);
+			void *p = setup_ALSA_mixer_elem(c->param1);
 			c->param1 = p;
 			break;
 #endif
-#ifdef HAVE_OSC
+		case JACK:
 		case OSC:
-			DBG("OSC controller required, but not yet implemented.");
-			break;
-#endif
 		case STDOUT:
 			break;
 		default:

commit 34e8a22564a9eaaddcf797bdf6036d69459f0a50

remove unnecessary variable, fixes compile failure introduced by 7379c179b74c617701507748c78a35ba7688f38f

main.c
@@ -185,8 +185,7 @@ int main(int argc, char *argv[])
 		switch (c->target) {
 #ifdef HAVE_ALSA
 		case ALSA:
-			void *p = setup_ALSA_mixer_elem(c->param1);
-			c->param1 = p;
+			c->param1 = setup_ALSA_mixer_elem(c->param1);
 			break;
 #endif
 		case JACK: