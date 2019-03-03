d1bb3388bc2afaf3f7db7f1b88f236c6617c1075
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2019-03-03 12:48:06 +0000

    explicitly cast values from and to (char*) to avoid compiler warning

--- ringbuffer.c
+++ ringbuffer.c
@@ -40,12 +40,12 @@ int ringbuffer_write(unsigned char *msg, size_t size)
 {
 	int nbytes;
 	pthread_mutex_lock(&buflock);
-	nbytes = jack_ringbuffer_write(buf, msg, size);
+	nbytes = jack_ringbuffer_write(buf, (char *)msg, size);
 	pthread_mutex_unlock(&buflock);
 	return nbytes;
 }
 
 int ringbuffer_read(unsigned char *msg, size_t size)
 {
-	return jack_ringbuffer_read(buf, msg, size);
+	return jack_ringbuffer_read(buf, (char *)msg, size);
 }
