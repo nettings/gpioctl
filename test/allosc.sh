58006d7677c52e848c597ea9d97fa77b04448988
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2019-05-10 18:30:57 +0100

    script for four-way test board

---
+++ test/allosc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# test board with four ALPS rotaries with switch
+
+../build/gpioctl\
+	 -r 27,17,osc,osc.udp://239.0.0.254:3000,/one/dial,0,100,1,0 \
+	 -r 23,24,osc,osc.udp://239.0.0.254:3000,/two/dial,0,100,1,0 \
+	 -r 8,7,osc,osc.udp://239.0.0.254:3000,/three/dial,0,100,1,0 \
+	 -r 12,16,osc,osc.udp://239.0.0.254:3000,/four/dial,0,100,1,0 \
+	 -s 13,osc,osc.udp://239.0.0.254:3000,/one/switch,1,0,1 \
+	 -s 22,osc,osc.udp://239.0.0.254:3000,/two/switch,1,0,1 \
+	 -s 25,osc,osc.udp://239.0.0.254:3000,/three/switch,0,0,1 \
+	 -s 26,osc,osc.udp://239.0.0.254:3000,/four/switch,0,0,1
